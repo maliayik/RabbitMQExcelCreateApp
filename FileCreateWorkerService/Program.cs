e29b9d75f63e2c1b788fcb66c69e17422d054fe9
maliiayik <maliiayik@gmail.com>
2024-05-27 20:13:26 +0300

    Worker Service

---
+++ FileCreateWorkerService/Program.cs
@@ -0,0 +1,29 @@
+using FileCreateWorkerService;
+using FileCreateWorkerService.Services;
+using RabbitMQ.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+      
+        IConfiguration configuration = hostContext.Configuration;
+
+        // RabbitMQ baðlantý ayarlarýný yapýlandýrma dosyasýndan çekiyoruz
+        var rabbitMqConnectionString = configuration.GetConnectionString("RabbitMQ");
+
+        // RabbitMQ baðlantý nesnesini oluþturup DI konteynerine ekliyoruz
+        services.AddSingleton(sp => new ConnectionFactory()
+        {
+            Uri = new Uri(rabbitMqConnectionString),
+            DispatchConsumersAsync = true
+        });
+
+        services.AddSingleton<RabbitMQClientService>();
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
